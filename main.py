75cfe103e49354f88d9e6e266833db4027ce2339
Christinne <vanessa0christinne@gmail.com>
2024-05-18 22:00:43 +0200

    Add alarm loop

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import clock_setup
 def main():
     print(clock_setup.get_current_time())
 
+    alarm = clock_setup.set_alarm()
+    print(alarm)
+
+    while True:
+        if clock_setup.check_alarm(alarm):
+            print("Alarm!")
+            break
+
 if __name__ == '__main__':
     main()
