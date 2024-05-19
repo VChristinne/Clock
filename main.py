0cce1b1ff9f533bd1a6754b10a57e79977dd40e9
Christinne <vanessa0christinne@gmail.com>
2024-05-19 12:22:29 +0200

    Add \r while loop

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import clock_setup
 from colorama import Fore
+from time import sleep
 
 def main():
-    clock_setup.get_current_time()
-
     alarm = clock_setup.set_alarm()
-    print(Fore.RED + f"\nAlarm set to: {alarm}" + Fore.RESET)
+    if alarm is None:
+        print("Invalid alarm setting. Please restart the program and choose a valid option.")
+        return
+
+    print(Fore.RED + "\nAlarm set to: " + Fore.RESET + f"{alarm}")
 
     while True:
-        if clock_setup.check_alarm(alarm):
-            print("\nAlarm!")
-            break
+        current_time = clock_setup.get_current_time()
+        remaining_time = clock_setup.time_remaining(alarm)
+
+        print(Fore.GREEN + "\rCurrent time: " + Fore.RESET + f"{current_time} | " + Fore.BLUE + "Time remaining: " + Fore.RESET + f"{remaining_time}", end="", flush=True)
+        sleep(1)
 
 if __name__ == '__main__':
     main()
